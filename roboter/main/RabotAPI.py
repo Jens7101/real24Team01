792e5872d1bc1f905e48ef64a873c5094ff94fd2
Lukas7243 <lukasbrandenburg@outlook.com>
2025-08-27 08:58:04 +0200

    Befohr anpassung vom auslesen der Distence sensoren

--- roboter/main/RabotAPI.py
+++ roboter/main/RabotAPI.py
@@ -1,11 +1,15 @@
 from Distance_Sensoren import*
-
+from mpu6050 import mpu6050
+from driver.vl53l0x_helper import init_vl53l0x
 
 
 class RabotAPI:
     
     def __init__(self):
         self.sensorwerte = [0] * 10
+
+        ## mpu6050 Sensor erstellen
+        self.mpuSensor = mpu6050(0x68)
         
     def getDistSensorValues(self):
         for i in range(len(werte)):  # bisher: range (16)
